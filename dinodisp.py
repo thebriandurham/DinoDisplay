714e6f73980d8107ac181f953ed6d32025e906c8
Brian Durham <47435950+thebriandurham@users.noreply.github.com>
2020-08-26 22:51:57 -0400

    Further dev work
    Not much progress

--- dinodisp.py
+++ dinodisp.py
@@ -6,10 +6,78 @@
 ##### Placeholder
 
 # Meta Vars
+DEBUG = True
 COMPORT = 0 # read from preferences.conf
 
+def test():
+
+	# Setup setps
+	if check_os() == True:
+		init_debug_mode()
+		init_error_log()
+		
+		init_open_hw_monitor()
+
+		print("dinodisp:test_program: COMPORT on OPEN: %s" % (COMPORT))
+
+		assign_comport()
+
+		print("dinodisp:test_program: COMPORT post ASSIGN: %s" % (COMPORT))
+
+		temp_data = get_ohm_temp_data()
+		display_string = process_temp_data_for_arduino(temp_data)
+
+		#print("dinodisp:test_program: temp_data: %s" % (temp_data))
+		#print("dinodisp:test_program: display_string: %s" % (display_string))
+
+		arduino_rx_tx(display_string)
+
+def arduino_rx_tx(display_string):
+	from time import sleep
+	import serial
+
+	comport_str = "COM" + str(COMPORT)
+
+	try:
+		serial_interface = serial.Serial(comport_str,9600,timeout=30)
+	except Exception as err:
+		write_error("dinodisp.py:arduino_rx_tx: Error opening COMPORT: %s; error(%s)" % (COMPORT,err))
+
+	try:
+		# DEBUG:
+		print("dinodisp.py:arduino_rx_tx: Sending message to Arduino: %s" % (display_string))
+
+		#serial_interface.write(display_string.encode())
+		#print(serial_interface.readline().decode())
+		#TESTING
+		display_chars = list(display_string)
+		#print(display_chars)
+		for char in display_chars:
+			print("dinodisp.py:arduino_rx_tx: Sending char %s to Arduino" % (char))
+			serial_interface.write(char.encode())
+			print("dinodisp.py:arduino_rx_tx: Awaiting Arduino response")
+			response = serial_interface.readline().decode()
+			print("dinodisp.py:arduino_rx_tx: Response received: %s" % (response))
+			sleep(.1)
+
+		# DEBUG:
+		# print("dinodisp.py:arduino_rx_tx: Arduino response: %s" % (response))
+
+		# if "OK" in response:
+		# 	sleep(.1)
+		# 	continue
+		# else:
+		# 	write_error("dinodisp.py:arduino_rx_tx: Received invalid response from Arduino device. Exiting")
+		# 	return False
+
+		#serial_io.close()
+		#serial_interface.close()
+
+	except Exception as err:
+		write_error("dinodisp.py:arduino_rx_tx: Error during send/receive to Arduino; error(%s)" %(err))
+		return False
+
 def process_temp_data_for_arduino(temp_data):
-	output_string = "" # Init output string for bytestream to arduino
 		
 	# Get average temp for CPUs with multiple cores
 	processed_temps = {}
@@ -29,21 +97,16 @@ def process_temp_data_for_arduino(temp_data):
 	for temp_dict,sub_values in processed_temps.items():
 		sub_values['avg_temp'] = int(sub_values['avg_temp'] / sub_values['num_temps'])
 
-	# Create the single line output string for the Arduino
+	display_string = ""
 	for temp_dict,sub_values in processed_temps.items():
-		temp_title = None
+		append_str = ""
 		if "cpu" in sub_values['tag']:
-			temp_title = "CPU"
+			append_str += "CPU: %s" % (sub_values['avg_temp'])
 		elif "gpu" in sub_values['tag']:
-			temp_title = "GPU"
-		else:
-			temp_title = sub_values['tag'] # Edge case for unknown make/manufacturer
-		output_string += "%s:%sC," % (temp_title,sub_values['avg_temp'])
+			append_str += "GPU: %s" % (sub_values['avg_temp'])
+		display_string += (append_str + ";")
 
-	# Terminator character for Arduino processing
-	output_string += "\n"
-	return output_string
-
+	return display_string
 
 def get_ohm_temp_data():
 	import wmi 
@@ -64,9 +127,6 @@ def get_ohm_temp_data():
 						"value":sensor.Value
 						})
 
-	## DEBUG : TODO: REMOVE THIS ONCE TESTING IS COMPLETE
-	print(temperatures)
-
 	return temperatures
 
 def init_open_hw_monitor():
@@ -81,8 +141,8 @@ def init_open_hw_monitor():
 	try:
 		cmd = ['OpenHardwareMonitor\OpenHardwareMonitor.exe']
 		process = subprocess.Popen(cmd, stdout = subprocess.PIPE, startupinfo=info)
-	except Exception as e:
-		write_error("dinodisp.py:init_open_hw_monitor: Could not open Open Hardware Monitor, is the file present/are you running as Admin?")
+	except Exception as err:
+		write_error("dinodisp.py:init_open_hw_monitor: Could not open Open Hardware Monitor, is the file present/are you running as Admin?; error(%s)" % (err))
 
 def assign_comport():
 	import re
@@ -94,11 +154,19 @@ def assign_comport():
 	matches = re.search(conf_regex,conf_file_contents, re.MULTILINE)
 
 	if matches.group(1):
+		global COMPORT
 		COMPORT = int(matches.group(1))
 	else:
 		write_error("dinodisp.py:assign_comport: Could not find comport integer in preferences.conf file")
 
 def write_error(error_message):
+	from datetime import datetime
+
+	error_message = str(datetime.now()) + ":" + error_message
+
+	if DEBUG:
+		print(error_message)
+
 	error_log = open("errors.txt","a")
 	error_log.write("\n%s" % (error_message))
 	error_log.close()
@@ -107,6 +175,10 @@ def init_error_log():
 	import os
 	if not os.path.exists("errors.txt"):
 		with open("errors.txt",'w'): pass
+
+def init_debug_mode():
+	global DEBUG
+	DEBUG = True
 
 def check_os():
 	import os
--- dinodisp/dinodisp.ino
+++ dinodisp/dinodisp.ino
@@ -10,21 +10,54 @@ const int d5_pin = 10;
 const int d6_pin = 11;
 const int d7_pin = 12;
 
+// LEARNING VARS TODO: DELETE
+String cpu_string;
+String gpu_string;
+const String valid_response = "OK\n";
+const String invalid_response = "ERR\n";
+
+char test_char = 'A';
+
 LiquidCrystal lcd(rs_pin,en_pin,d4_pin,d5_pin,d6_pin,d7_pin);
 
 void setup() {
+
+  // DEBUG Functioning Indicator Use
+  pinMode(LED_BUILTIN, OUTPUT);
+  
   Serial.begin(9600);
   delay(SETUP_TIME);
   lcd.begin(16,2);
 }
 
 void loop() {
+    
+//  while( Serial.available() == 0){
+//    // DEBUG Functioning Indicator
+//    // Blink while waiting for serial input!
+//    digitalWrite(LED_BUILTIN,HIGH);
+//    delay(500);
+//    digitalWrite(LED_BUILTIN,LOW);
+//    delay(500);
+//  }
 
-  // TODO: Fill this out
-  // Print to LCD
+  if (Serial.available() > 0){
+    char inByte = Serial.read();
+    lcd.clear();
+    lcd.print("RCV");
+    Serial.print("OK");
+  }
+
   lcd.setCursor(0,0);
-  lcd.print("Hello World");
-
-  delay(1000);
   lcd.clear();
+  lcd.print("NO RCV");
+
+  delay(100);
+
+ // cpu_string = Serial.readString();
+  //lcd.setCursor(0,0);
+ // lcd.print(cpu_string);
+    // Print to LCD
+   // lcd.setCursor(0,1);
+    //lcd.print(gpu_string);
 }
